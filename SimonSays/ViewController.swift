96c56723159850f49fda8e38d08473bbef975678
Joe <Kcpx922@gmail.com>
2021-03-28 18:24:39 -0700

    Initial Commit

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Shihchiao Chao on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
